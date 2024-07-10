99c32d859688d78763353dc3534b73f832e1859c
Jonathan Marshall <jwmarsh16@gmail.com>
2024-07-10 17:47:58 -0500

    added author ORM methods

--- lib/models/author.py
+++ lib/models/author.py
@@ -1,5 +1,27 @@
 from models.__init__ import CONN, CURSOR
 
 class Author:
+    def __init__(self, id, name, birth_date):
+        self.id = id
+        self.name = name
+        self.birth_date = birth_date
 
-    pass
+    @classmethod
+    def create(cls, name, birth_date):
+
+        pass
+
+    @classmethod
+    def delete(cls, author_id):
+
+        pass
+
+    @classmethod
+    def get_all(cls):
+
+        pass
+
+    @classmethod
+    def get_by_id(cls, author_id):
+
+        pass

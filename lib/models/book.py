391b0c6635456c1e7b6ad8e91e87951460d0f6e1
Jonathan Marshall <jwmarsh16@gmail.com>
2024-07-10 17:42:22 -0500

    added project structure and book/author attributes

--- lib/models/book.py
+++ lib/models/book.py
@@ -1,5 +1,11 @@
 from models.__init__ import CONN, CURSOR
 
 class Book:
+    def __init__(self, id, title, published_date, author_id, available=True):
+        self.id = id
+        self.title = title
+        self.published_date = published_date
+        self.author_id = author_id
+        self.available = available
 
     pass
